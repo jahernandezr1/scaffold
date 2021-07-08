1fb6c9a780d6c2d8f2b64936d89fc5d56edc6291
jahernandezr1 <82291476+jahernandezr1@users.noreply.github.com>
2021-07-08 09:56:13 -0500

    Update hello.py
    cambio en funciones

--- hello.py
+++ hello.py
@@ -1,7 +1,11 @@
-def add(x, y):
-    return x + y
+def toyou(x):
+    return f"hi {x}"
 
+def add(x):
+    return x + 1
+
+def subtract(x):
+    return x - 1
 
 x1 = 2
-y1 = 2
-print(f"This is the sum: {x1}, {y1}, {add(x1,y1)}")
+print(f"This is the sum: {x1}, {add(x1)}")

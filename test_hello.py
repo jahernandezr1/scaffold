b69db9a3ad347aafd683eda58524b294274f3d09
jahernandezr1 <82291476+jahernandezr1@users.noreply.github.com>
2021-07-09 14:07:34 -0500

    Update test_hello.py

--- test_hello.py
+++ test_hello.py
@@ -1,14 +1,14 @@
 from hello import toyou, add, subtract
 
 
-def setup_function(function):
-    print(f" Running Setup: {function.__name__}")
-    function.x = 10
+#def setup_function(function):
+#    print(f" Running Setup: {function.__name__}")
+#    function.x = 10
 
 
-def teardown_function(function):
-    print(f" Running Teardown: {function.__name__}")
-    del function.x
+#def teardown_function(function):
+#    print(f" Running Teardown: {function.__name__}")
+#    del function.x
 
 
 ### Run to see failed test
@@ -16,4 +16,4 @@ def teardown_function(function):
 #    assert add(test_hello_add.x) == 12
 
 def test_hello_subtract():
-    assert subtract(test_hello_subtract.x) == 9
+    assert subtract(10) == 9
